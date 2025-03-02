b0b9668232b893a3ed2d497d26e946596cba79d5
Aleksandr Kochetov <alexs.kochetov@gmail.com>
2025-03-02 11:53:08 +0500

    unique user email

---
+++ src/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+
+from src.database import Base
+
+
+class UsersORM(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(200), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(200))
+    name: Mapped[str] = mapped_column(String(50))
