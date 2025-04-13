3fd924bc3bb50a62754cdd497a915c95998cb8b9
Aleksandr Kochetov <alexs.kochetov@gmail.com>
2025-04-13 13:04:41 +0500

    new handlers for bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from datetime import date
 
-from src.api.dependencies import get_current_user_id, get_token, UserIdDep
+from src.api.dependencies import UserIdDep
 from src.api.dependencies import DBDep
 from src.schemas.bookings import AddBookingRequest, AddBooking
 
@@ -14,13 +13,22 @@ async def create_booking(
         db: DBDep,
         user_id: UserIdDep,
 ):
-
-    price = (await db.rooms.get_one_or_none(id=booking_data.room_id)).price
+    room_price = (await db.rooms.get_one_or_none(id=booking_data.room_id)).price
     _booking_data = AddBooking(
         user_id=user_id,
-        price=price,
+        price=room_price,
         **booking_data.model_dump()
     )
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
+
+
+@router.get("")
+async def get_all_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+
+@router.get("/me")
+async def get_user_bookings(user_id: UserIdDep, db: DBDep, ):
+    return await db.bookings.get_filtered(user_id=user_id)
